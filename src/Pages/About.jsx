821d32dafb1554e9f4231cf6971ec819e2210de0
Roshan <neelamsairoshan2003@gmail.com>
2025-08-25 10:39:53 +0530

    resume drive link updated

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -299,7 +299,7 @@ const AboutPage = () => {
 
             {/* Action Buttons */}
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-              <a href="https://drive.google.com/drive/folders/1BOm51Grsabb3zj6Xk27K-iRwI1zITcpo" className="w-full lg:w-auto">
+              <a href="https://drive.google.com/file/d/1ppDd2kIP1VZFA6WktZZs4erR0T9g2QN-/view?usp=sharing" className="w-full lg:w-auto">
               <button 
                 data-aos="fade-up"
                 data-aos-duration="800"
